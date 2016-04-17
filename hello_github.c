34158bb57f1a18e11725b9ef2d1f4289f0e93375
kissiey <kissiey@hotmail.com>
2016-04-17 22:38:36 +0800

    add hello_github.c

---
+++ hello_github.c
@@ -0,0 +1,22 @@
+/* ************************************************************************
+ *       Filename:  hello_github.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  2016年04月17日 22时37分11秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ * ************************************************************************/
+
+#include <iostd.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+	printf("hello, github\n");
+	return 0;
+}
+
+
